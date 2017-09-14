8d249a21b52a0c6a24dac70cc4d1a35946c38187
Brandon Zarrella <zarrellab@gmail.com>
2017-09-14 13:05:37 -0600

    Catch no results

--- src/Components/RegionKillList.jsx
+++ src/Components/RegionKillList.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
 export default function RegionKillList({ data }) {
+  function listData() {
+    try {
+      return (
+        data.map(region => (
+          <li key={region._id.regionName}>{region._id.regionName} - {region.total}</li>
+        ))
+      )
+    } catch (e) {
+      return <h1>No Results</h1>
+    }
+  }
   return (
     <ul>
-      {data.map(region => (
-        <li key={region._id.regionName}>{region._id.regionName} - {region.total}</li>
-      ))}
+      {listData()}
     </ul>
   )
 }
