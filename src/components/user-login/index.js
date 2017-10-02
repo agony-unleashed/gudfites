51aa6b9c3f4a06ab4f2d4f2eb9bbf8ded81d95ce
Brandon Zarrella <zarrellab@gmail.com>
2017-10-02 11:22:36 -0600

    show user simple error on wrong password

--- src/components/user-login/index.js
+++ src/components/user-login/index.js
@@ -1,29 +1,63 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import LoginForm from './login-form'
 
-export default function UserLogin ({ handleLogin }) {
-  const storeToken = token => { localStorage.setItem('gudfitesAccessToken', token) }
+const storeToken = token => { localStorage.setItem('gudfitesAccessToken', token) }
+export default class UserLogin extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      password: '',
+      loginError: false,
+    }
 
-  return (
-    <div>
-      <LoginForm onSubmit={e => _attemptLogin(e)} />
-    </div>
-  )
+    this.handleLogin = this.props.handleLogin.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleLoginError = this.handleLoginError.bind(this)
+    this._attemptLogin = this._attemptLogin.bind(this)
+  }
+
+  render () {
+    const { loginError, password } = this.state
+    return (
+      <div>
+        <LoginForm
+          loginError={loginError}
+          password={password}
+          onChange={e => this.handleChange(e)}
+          onSubmit={e => this._attemptLogin(e)}
+        />
+      </div>
+    )
+  }
 
   // -------------------------------------------------------------------------
 
-  function _attemptLogin (e) {
+  handleChange(e) {
+    this.setState({ password: e.target.value })
+  }
+
+  handleLoginError() {
+    this.setState({
+      loginError: true,
+      password: '',
+    })
+  }
+
+  _attemptLogin (e) {
     e.preventDefault()
     fetch('https://api.gudfites.com/auth', {
       method: 'POST',
       body: new FormData(e.currentTarget)
     })
-      .then(res => res.json())
+      .then(res => res.ok
+        ? res.json()
+        : this.handleLoginError()
+      )
       .then(res => {
         storeToken(res.token)
 
-        handleLogin(true)
+        this.handleLogin(true)
       })
       .catch(console.error)
   }
--- src/components/user-login/login-form/index.js
+++ src/components/user-login/login-form/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
-export default function LoginForm ({ onSubmit }) {
+import '../../../static/login-form.css'
+
+export default function LoginForm ({ onChange, onSubmit, loginError, password }) {
   return (
     <form className="text-center" onSubmit={onSubmit} name='login'>
-      <input type='password' name='password' placeholder='password' required />
+      <input
+        className={ loginError ? 'login-error' : '' }
+        onChange={onChange}
+        type='password'
+        name='password'
+        placeholder={ loginError ? 'wrong password' : 'password...' }
+        value={password}
+        required
+      />
 
       <button>Access</button>
     </form>
